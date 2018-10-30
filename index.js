dd020d8b025a4ac9c339f41b738c5effb9e82c75
Pete Houston <contact@petehouston.com>
2018-10-30 22:25:14 +0700

    Load main component.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/main';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('carBooking', () => App);
